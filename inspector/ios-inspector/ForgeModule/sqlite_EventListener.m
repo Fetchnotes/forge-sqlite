9a02221b89e0051e2b775201568331f5c698af73
Alex Horak <alexamh9@gmail.com>
2013-11-04 19:16:20 -0500

    Untested but clean

--- inspector/ios-inspector/ForgeModule/sqlite_EventListener.m
+++ inspector/ios-inspector/ForgeModule/sqlite_EventListener.m
@@ -11,37 +11,18 @@
 
 + (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
-    #if !(TARGET_IPHONE_SIMULATOR)
-    
     NSString *token = [[deviceToken description] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
     token = [token stringByReplacingOccurrencesOfString:@" " withString:@""];
-
-
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Device Token"
-                                                    message:token
-                                                   delegate:nil
-                                          cancelButtonTitle:@"OK"
-                                          otherButtonTitles:nil];
-    [alert show];
 
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *docsPath = [paths objectAtIndex:0];
     NSString *path = [docsPath stringByAppendingPathComponent:@"temp-database.sqlite"];
     BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:path];
-    
-    NSString * booleanString = (fileExists) ? @"True" : @"False";
-
-    UIAlertView *alert2 = [[UIAlertView alloc] initWithTitle:@"temp-database exists?"
-                                                    message: booleanString
-                                                   delegate:nil
-                                          cancelButtonTitle:@"OK"
-                                          otherButtonTitles:nil];
-    [alert2 show];
 
     if (fileExists == NO) {
         // destroy and recreate
-//            NSFileManager *fileManager = [NSFileManager defaultManager];
-//            [fileManager removeItemAtPath:path error:NULL];
+        // NSFileManager *fileManager = [NSFileManager defaultManager];
+        // [fileManager removeItemAtPath:path error:NULL];
         FMDatabase *database = [FMDatabase databaseWithPath:path];
         
         [database open];
@@ -49,24 +30,11 @@
         [database executeUpdate:@"INSERT into temp(DeviceToken) VALUES (?)", token];
         [database close];
     }
-    
-    #endif
 }
 
-+ (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
-{
-    [ForgeLog i:@"Failed to register for remote notifications"];
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"failed to register for push notifs"
-                                                    message:[error description]
-                                                   delegate:nil
-                                          cancelButtonTitle:@"OK"
-                                          otherButtonTitles:nil];
-    [alert show];
-}
++ (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {}
 
-+ (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
-{
-}
++ (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {}
 
 // The example below passes an event through to JavaScript when the application is resumed.
 + (void)applicationWillEnterForeground:(UIApplication *)application {
